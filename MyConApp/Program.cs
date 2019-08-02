ce9dd145adc043c10923f518738e8157cfd148cd
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-02 13:11:42 +0430

    Consumer

---
+++ MyConApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace MyConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "orders",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: "orders",
+                                    false, consumer);
+
+                //while (true)
+                //{
+
+                //};
+
+
+                //Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+    }
+}
