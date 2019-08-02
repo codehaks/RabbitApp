3151284960be61b9ff75d6a01b6934e4087e733b
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-02 15:23:18 +0430

    Fix

---
+++ MyOrderConsumer/Startup.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MyOrderConsumer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<RabbitListener>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRabbitListener();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+
+    public class RabbitListener
+    {
+        ConnectionFactory factory { get; set; }
+        IConnection connection { get; set; }
+        IModel channel { get; set; }
+
+        public void Register()
+        {
+            channel.QueueDeclare(queue: "orders", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                int m = 0;
+            };
+            channel.BasicConsume(queue: "orders", autoAck: false, consumer: consumer);
+        }
+
+        public void Deregister()
+        {
+            this.connection.Close();
+        }
+
+        public RabbitListener()
+        {
+            this.factory = new ConnectionFactory() { HostName = "localhost" };
+            this.connection = factory.CreateConnection();
+            this.channel = connection.CreateModel();
+
+
+        }
+    }
+    public static class ApplicationBuilderExtentions
+    {
+        //the simplest way to store a single long-living object, just for example.
+        private static RabbitListener _listener { get; set; }
+
+        public static IApplicationBuilder UseRabbitListener(this IApplicationBuilder app)
+        {
+            _listener = app.ApplicationServices.GetService<RabbitListener>();
+
+            var lifetime = app.ApplicationServices.GetService<IApplicationLifetime>();
+
+            lifetime.ApplicationStarted.Register(OnStarted);
+
+            //press Ctrl+C to reproduce if your app runs in Kestrel as a console app
+            lifetime.ApplicationStopping.Register(OnStopping);
+
+            return app;
+        }
+
+        private static void OnStarted()
+        {
+            _listener.Register();
+        }
+
+        private static void OnStopping()
+        {
+            _listener.Deregister();
+        }
+    }
+}
